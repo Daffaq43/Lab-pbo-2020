2c33ae4c785c09e93fd3232b0ae917298315582f
daps <61241610+Daffaq43@users.noreply.github.com>
2021-01-04 19:13:48 +0700

    membuat kode pertemuan 7

---
+++ pertemuan 7/CoinFlip.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class CoinFlip {
+     public static void main(String[] args){
+         // 50% chance heads, 50% chance tails
+         Random rand = new Random();//untuk deklarasi keyword random
+         double chance = rand.nextDouble();//untuk mengacak
+         if (chance < 0.5) {
+             System.out.println("heads!");
+         } else {
+             System.out.println("tails!");
+         }
+     }
+}
